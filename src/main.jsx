54792e57ad52107830e8bffaf35cb18f86f4789d
dannyreiser <dannyreiser@gmail.com>
2026-01-13 21:35:48 +0200

    Replace BrowserRouter with HashRouter in main.jsx for improved routing compatibility in various environments.

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import { MusicPlayerProvider } from './contexts/MusicPlayerContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <MusicPlayerProvider>
         <App />
       </MusicPlayerProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
